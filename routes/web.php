a7ebde7e70ff8bfac1df5d2299a1159a9c9a0594
Rvondc <rogivon.delacruz@gmail.com>
2024-05-14 20:16:27 +0800

    refactor: update routes for login and logout functionality
    This commit updates the routes defined in routes/web.php to handle login and logout functionality.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('auth.login');
+// });
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'loginAuth']);
+});
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::controller(AuthController::class)->group(function () {
+        Route::get('/logout', 'logout');
+    });
 });
