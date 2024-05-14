520260374bd48a37461775c93f63108d9ed0d2bf
Rvondc <rogivon.delacruz@gmail.com>
2024-05-14 19:44:28 +0800

    feat: add initial user to DatabaseSeeder.php
    This commit modifies the DatabaseSeeder.php file to add an initial user record during database seeding.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'admin@email.com',
+            'profile_photo' => null,
+            'password' => Hash::make('password'),
+            'gender_id' => 1,
+            'role_id' => 1,
         ]);
     }
 }
